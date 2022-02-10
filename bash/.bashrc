25ac1dece28eb5c0d6162497ff97453c554567b7
Nemanja Lakicevic <nemanja.lakicevic@statnett.no>
2022-02-10 11:10:02 +0100

    bashrc fix;

--- bash/.bashrc
+++ bash/.bashrc
@@ -2,32 +2,27 @@
 # Minimal devbox .bashrc. Add user custom stuff
 # ##############################################
 
-
-source ${HOME}/dotfiles/scripts/exports-common.sh
-# export PNPM_STORE="${XDG_LIB_HOME}/pnpm-store"
-# export PNPM_GLOBAL="${XDG_LIB_HOME}/pnpm-global"
-# export PNPM_GLOBAL_BIN="${XDG_BIN_HOME}/pnpm-global"
-# export PNPM_STATE="${XDG_DATA_HOME}/pnpm-state"
-# export XDG_DATA_HOME=${HOME}/.local/share
-# export XDG_CONFIG_HOME=${HOME}/.config
-# export XDG_CACHE_HOME=${HOME}/.local/cache
-# export DOTFILES=${HOME}/dotfiles
-# export NVIM_CONFIG_HOME=${DOTFILES}/nvim/.config/nvim
-# export NVIM_CONFIG=${NVIM_CONFIG_HOME}/init.vim
-# export NELAPYS=${XDG_DATA_HOME}/nelapys
+export XDG_DATA_HOME=${HOME}/.local/share
+export XDG_CONFIG_HOME=${HOME}/.config
+export XDG_CACHE_HOME=${HOME}/.local/cache
+export XDG_BIN_HOME=${HOME}/.local/bin
+export DOTS=${HOME}/dotfiles
+export REPOS=${HOME}/repos
+export NVIM=${DOTS}/nvim/.config/nvim
+export NVIM_CONFIG=${NVIM_CONFIG_HOME}/init.vim
+export NELAPYS=${XDG_DATA_HOME}/nelapys
 export POETRY_VIRTUALENVS_PATH=${NELAPYS}
+
+[ ":${PATH}:" != *":${XDG_BIN_HOME}:"* ]  && export PATH="${XDG_BIN_HOME}:${PATH}"
+
+source ~/.asdf/plugins/java/set-java-home.bash
 
 # Add this lines at the top of .bashrc:
 [[ $- == *i* ]] && source ${HOME}/repos/ble.sh/out/ble.sh --noattach
 
-source ${DOTS}/scripts/asdf-pyvirtual-envs.sh
-
-source ${DOTS}/fzf/fzf.bash
-source ${DOTS}/scripts/bash-it
-source ${DOTS}/scripts/aliases.sh
-source ${DOTS}/scripts/locale.sh
-#source $DOTFILES/scripts/install-azure-cli.sh
-
+source $DOTS/asdf/asdf-pyvirtual-envs.sh
+source $DOTS/scripts/fzf
+source $DOTS/scripts/bash-it
 
 # Add this line at the end of .bashrc:
 # [[ ${BLE_VERSION-} ]] && ble-attach
--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/nela/.local/cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/nela/.local/cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/nela/.local/cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/nela/.local/cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/nela/.local/cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/nemanjl/.local/cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/nemanjl/.local/cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/nemanjl/.local/cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/nemanjl/.local/cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/nemanjl/.local/cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -72,132 +72,114 @@ _G.packer_plugins = {
   ["Comment.nvim"] = {
     config = { "\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0" },
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/Comment.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
   },
   ["DAPInstall.nvim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/DAPInstall.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/DAPInstall.nvim",
     url = "https://github.com/Pocco81/DAPInstall.nvim"
   },
   LuaSnip = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-cmdline"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
     url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-omni"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/cmp-omni",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/cmp-omni",
     url = "https://github.com/hrsh7th/cmp-omni"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["gruvbox-material"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/gruvbox-material",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/gruvbox-material",
     url = "https://github.com/sainnhe/gruvbox-material"
   },
   ["lightspeed.nvim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/lightspeed.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/lightspeed.nvim",
     url = "https://github.com/ggandor/lightspeed.nvim"
   },
   ["lspkind-nvim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/lspkind-nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/lspkind-nvim",
     url = "https://github.com/onsails/lspkind-nvim"
   },
   ["lualine-lsp-progress"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/opt/lualine-lsp-progress",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/opt/lualine-lsp-progress",
     url = "https://github.com/arkav/lualine-lsp-progress"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
-  },
-  ["markdown-preview.nvim"] = {
-    commands = { "MarkdownPreview" },
-    config = { "\27LJ\2\n\v\0\0\1\0\0\0\1K\0\1\0\0" },
-    loaded = false,
-    needs_bufread = false,
-    only_cond = false,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/opt/markdown-preview.nvim",
-    url = "https://github.com/iamcco/markdown-preview.nvim"
   },
   ["nvim-autopairs"] = {
     config = { "\27LJ\2\nb\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\14fast_wrap\1\0\0\1\0\1\bmap\n<M-w>\nsetup\19nvim-autopairs\frequire\0" },
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
-  },
-  ["nvim-colorizer.lua"] = {
-    commands = { "ColorizerAttachToBuffer" },
-    config = { "\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0" },
-    loaded = false,
-    needs_bufread = false,
-    only_cond = false,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/opt/nvim-colorizer.lua",
-    url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-dap"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/nvim-dap",
     url = "https://github.com/mfussenegger/nvim-dap"
   },
   ["nvim-jdtls"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/nvim-jdtls",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/nvim-jdtls",
     url = "https://github.com/mfussenegger/nvim-jdtls"
   },
   ["nvim-lsp-installer"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
     url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
@@ -206,102 +188,79 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/opt/nvim-tree.lua",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/opt/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/opt/packer.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/opt/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   playground = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/playground",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/playground",
     url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
-  },
-  scnvim = {
-    config = { "\27LJ\2\n&\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\vscnvim\frequire\0" },
-    loaded = false,
-    needs_bufread = true,
-    only_cond = false,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/opt/scnvim",
-    url = "https://github.com/davidgranstrom/scnvim"
   },
   ["targets.vim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/targets.vim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/targets.vim",
     url = "https://github.com/wellle/targets.vim"
   },
   ["telescope-fzf-native.nvim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
     url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
   },
   ["telescope-symbols.nvim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/telescope-symbols.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/telescope-symbols.nvim",
     url = "https://github.com/nvim-telescope/telescope-symbols.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-easy-align"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/vim-easy-align",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/vim-easy-align",
     url = "https://github.com/junegunn/vim-easy-align"
   },
   ["vim-eunuch"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/vim-eunuch",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/vim-eunuch",
     url = "https://github.com/tpope/vim-eunuch"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-qf"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/vim-qf",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/vim-qf",
     url = "https://github.com/romainl/vim-qf"
-  },
-  ["vim-solidity"] = {
-    loaded = false,
-    needs_bufread = true,
-    only_cond = false,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/opt/vim-solidity",
-    url = "https://github.com/tomlion/vim-solidity"
   },
   ["vim-surround"] = {
     loaded = true,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/start/vim-surround",
+    path = "/home/nemanjl/.local/share/nvim/site/pack/packer/start/vim-surround",
     url = "https://github.com/tpope/vim-surround"
-  },
-  vimtex = {
-    commands = { "VimtexCompile", "VimtexView" },
-    loaded = false,
-    needs_bufread = true,
-    only_cond = false,
-    path = "/Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex",
-    url = "https://github.com/lervag/vimtex"
   }
 }
 
@@ -310,51 +269,20 @@ time([[Defining packer_plugins]], false)
 time([[Setup for nvim-tree.lua]], true)
 try_loadstring("\27LJ\2\n0\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\21nelavim.nvimtree\frequire\0", "setup", "nvim-tree.lua")
 time([[Setup for nvim-tree.lua]], false)
--- Config for: nvim-autopairs
-time([[Config for nvim-autopairs]], true)
-try_loadstring("\27LJ\2\nb\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\14fast_wrap\1\0\0\1\0\1\bmap\n<M-w>\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
-time([[Config for nvim-autopairs]], false)
 -- Config for: Comment.nvim
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\nb\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\14fast_wrap\1\0\0\1\0\1\bmap\n<M-w>\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
 
 -- Command lazy-loads
 time([[Defining lazy-load commands]], true)
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file VimtexCompile lua require("packer.load")({'vimtex'}, { cmd = "VimtexCompile", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file MarkdownPreview lua require("packer.load")({'markdown-preview.nvim'}, { cmd = "MarkdownPreview", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file NvimTreeToggle lua require("packer.load")({'nvim-tree.lua'}, { cmd = "NvimTreeToggle", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file ColorizerAttachToBuffer lua require("packer.load")({'nvim-colorizer.lua'}, { cmd = "ColorizerAttachToBuffer", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file VimtexView lua require("packer.load")({'vimtex'}, { cmd = "VimtexView", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 time([[Defining lazy-load commands]], false)
 
-vim.cmd [[augroup packer_load_aucmds]]
-vim.cmd [[au!]]
-  -- Filetype lazy-loads
-time([[Defining lazy-load filetype autocommands]], true)
-vim.cmd [[au FileType supercollider ++once lua require("packer.load")({'scnvim'}, { ft = "supercollider" }, _G.packer_plugins)]]
-vim.cmd [[au FileType sol ++once lua require("packer.load")({'vim-solidity'}, { ft = "sol" }, _G.packer_plugins)]]
-vim.cmd [[au FileType tex ++once lua require("packer.load")({'vimtex'}, { ft = "tex" }, _G.packer_plugins)]]
-vim.cmd [[au FileType md ++once lua require("packer.load")({'markdown-preview.nvim'}, { ft = "md" }, _G.packer_plugins)]]
-time([[Defining lazy-load filetype autocommands]], false)
-vim.cmd("augroup END")
-vim.cmd [[augroup filetypedetect]]
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/scnvim/ftdetect/supercollider.vim]], true)
-vim.cmd [[source /Users/nela/.local/share/nvim/site/pack/packer/opt/scnvim/ftdetect/supercollider.vim]]
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/scnvim/ftdetect/supercollider.vim]], false)
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/vim-solidity/ftdetect/solidity.vim]], true)
-vim.cmd [[source /Users/nela/.local/share/nvim/site/pack/packer/opt/vim-solidity/ftdetect/solidity.vim]]
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/vim-solidity/ftdetect/solidity.vim]], false)
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/cls.vim]], true)
-vim.cmd [[source /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/cls.vim]]
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/cls.vim]], false)
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/tex.vim]], true)
-vim.cmd [[source /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/tex.vim]]
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/tex.vim]], false)
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/tikz.vim]], true)
-vim.cmd [[source /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/tikz.vim]]
-time([[Sourcing ftdetect script at: /Users/nela/.local/share/nvim/site/pack/packer/opt/vimtex/ftdetect/tikz.vim]], false)
-vim.cmd("augroup END")
 if should_profile then save_profiles() end
 
 end)
