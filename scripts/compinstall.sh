5fbf6d4cb0e97f863cb030f0e59671347beada10
Nemanja Lakicevic <n@lakicevic.no>
2022-10-31 09:21:09 +0100

    deleted unnecessary stuff

--- scripts/compinstall.sh
+++
@@ -1,8 +0,0 @@
-# The following lines were added by compinstall
-
-zstyle :compinstall filename '${DOTS}/zsh/zsh-completion'
-autoload -Uz compinit
-compinit
-ZSH_DISABLE_COMPFIX="true"
-
-# End of lines added by compinstall
--- scripts/todo-init.sh
+++
@@ -1,5 +0,0 @@
-# todo
-alias todo="todo.sh"
-if [ -f /usr/local/Cellar/todo-txt/2.12.0/etc/bash_completion.d/todo_completion ]; then
-  . /usr/local/Cellar/todo-txt/2.12.0/etc/bash_completion.d/todo_completion
-fi
