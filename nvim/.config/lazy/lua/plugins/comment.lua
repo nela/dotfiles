43597b5b4aed40846981be6d4f2e89122df56ea0
Nemanja Lakicevic <n@lakicevic.no>
2023-09-05 09:07:00 +0200

    feat: nvim comment

---
+++ nvim/.config/lazy/lua/plugins/comment.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'numToStr/Comment.nvim',
+    --keys = { 'gcc', 'gcb' }
+    -- event = "VeryLazy"
+    keys = {
+      { "gcc", mode = "n",          desc = "Comment toggle current line" },
+      { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+      { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+      { "gbc", mode = "n",          desc = "Comment toggle current block" },
+      { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+      { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
+    },
+    config = function(_, opts_)
+      require("Comment").setup(opts)
+    end
+
+    --lazy = false
+  }
+}
