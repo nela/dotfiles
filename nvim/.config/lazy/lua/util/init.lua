acf26ff752b7452ae632dd4cd0a08c42a75c0bec
Nemanja Lakicevic <n@lakicevic.no>
2023-09-18 19:03:48 +0200

    chore: utils updates

--- nvim/.config/lazy/lua/util/init.lua
+++ nvim/.config/lazy/lua/util/init.lua
@@ -20,11 +20,7 @@ end
 ---@param name string
 function M.opts(name)
   local plugin = require("lazy.core.config").plugins[name]
-  print(plugin)
-  if not plugin then
-    print("empty")
-    return {}
-  end
+  if not plugin then return {} end
   local Plugin = require("lazy.core.plugin")
   return Plugin.values(plugin, "opts", false)
 end
