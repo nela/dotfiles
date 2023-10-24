36fba1e07b383869b45070f27e8dd05a4ee95acb
Nemanja Lakicevic <n@lakicevic.no>
2023-10-24 13:12:49 +0200

    feat: fzf cycle

--- shells/fzf.sh
+++ shells/fzf.sh
@@ -1,6 +1,6 @@
 export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow"
 export FZF_COMPLETION_TRIGGER=",,"
-export FZF_DEFAULT_OPTS="--no-mouse --layout=reverse --multi --info=inline --bind 'ctrl-y:execute-silent(echo {+} | ${clip} ),ctrl-j:half-page-down,ctrl-k:half-page-up'"
+export FZF_DEFAULT_OPTS="--cycle --no-mouse --layout=reverse --multi --info=inline --bind 'ctrl-y:execute-silent(echo {+} | ${clip} ),ctrl-j:half-page-down,ctrl-k:half-page-up'"
 export FZF_COMPLETION_OPTS='--info=inline'
 
 preview="--preview=' [[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --number {} || cat {}) 2> /dev/null | head -n 300' --preview-window='wrap' --bind 'f3:execute(bat -style=numbers {} || less -f {}),f2:toggle-preview,ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up,ctrl-a:select-all+accept'"
