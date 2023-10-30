7e82bd12c65a4ac66d337ab98fbbd64480783249
Nemanja Lakicevic <n@lakicevic.no>
2023-10-30 19:38:00 +0100

    feat: add svete ls

---
+++ nvim/.config/lazy/lua/ft/svelte.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "svelte" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        svelte = {
+          cmd = { "svelteserver", "--stdio" },
+          filetypes = { "svelte" }
+        }
+      },
+    }
+  }
+}
