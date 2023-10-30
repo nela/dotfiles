b606c3e9bdba387ea15d2355caa02cbf200e83d0
Nemanja Lakicevic <n@lakicevic.no>
2023-10-30 19:37:49 +0100

    chore: lsp css rules validation

--- nvim/.config/lazy/lua/ft/css.lua
+++ nvim/.config/lazy/lua/ft/css.lua
@@ -1,3 +1,5 @@
+local util = require("lspconfig.util")
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -13,10 +15,35 @@ return {
       servers = {
         cssls = {
           settings = {
-            css = { validate = true },
-            scss = { validate = true },
+            css = {
+              lint = { unknownAtRules = "ignore" },
+              validate = true },
+            scss = {
+              lint = { unknownAtRules = "ignore" },
+              validate = true
+            },
           },
           cmd = { "vscode-css-language-server", "--stdio" }
+        },
+        tailwindcss = {
+          settings = {
+            tailwindCSS = {
+              classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
+              lint = {
+                cssConflict = "warning",
+                invalidApply = "error",
+                invalidConfigPath = "error",
+                invalidScreen = "error",
+                invalidTailwindDirective = "error",
+                invalidVariant = "error",
+                recommendedVariantOrder = "warning"
+              },
+              validate = true
+            }
+          },
+          cmd = { "tailwindcss-language-server", "--stdio" },
+          filetypes = { "svelte", "css", "scss", "html", "postcss", "javascript", "typescript" },
+          root_dir = util.root_pattern('tailwind.config.js', 'tailwind.config.cjs', 'tailwind.config.mjs', 'tailwind.config.ts', 'postcss.config.js', 'postcss.config.cjs', 'postcss.config.mjs', 'postcss.config.ts', 'package.json')
         }
       }
     }
