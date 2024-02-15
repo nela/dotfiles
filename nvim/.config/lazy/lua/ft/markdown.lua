8e6dd5a1a08168b0fb2b2bbf30e7f59543498ec1
Nemanja Lakicevic <n@lakicevic.no>
2024-02-15 08:49:39 +0100

    feat: add markdown preview

---
+++ nvim/.config/lazy/lua/ft/markdown.lua
@@ -0,0 +1,13 @@
+return {
+  -- install with yarn or npm
+  {
+    "iamcco/markdown-preview.nvim",
+    disable = true,
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  }
+}
