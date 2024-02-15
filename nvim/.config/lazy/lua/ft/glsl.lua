0576f7673a30365bbbbf365831a995012f8a4e1b
Nemanja Lakicevic <n@lakicevic.no>
2024-02-15 08:50:08 +0100

    feat: glsl server conf

---
+++ nvim/.config/lazy/lua/ft/glsl.lua
@@ -0,0 +1,22 @@
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "glsl" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        glsl_analyzer = {
+          cmd = { os.getenv('XDG_DATA_HOME') .. "/lsp/glsl_analyzer/bin/glsl_analyzer"},
+          filetypes = { "glsl", "vert", "frag" }
+        }
+      },
+    }
+  }
+}
