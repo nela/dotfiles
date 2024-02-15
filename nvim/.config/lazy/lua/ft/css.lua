d6fcf3b0ad999e2d02f61439d58c51543545681d
Nemanja Lakicevic <n@lakicevic.no>
2024-02-15 08:46:45 +0100

    chore: remove tailwind ls

--- nvim/.config/lazy/lua/ft/css.lua
+++ nvim/.config/lazy/lua/ft/css.lua
@@ -1,5 +1,3 @@
-local util = require("lspconfig.util")
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -25,26 +23,6 @@ return {
           },
           cmd = { "vscode-css-language-server", "--stdio" }
         },
-        tailwindcss = {
-          settings = {
-            tailwindCSS = {
-              classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
-              lint = {
-                cssConflict = "warning",
-                invalidApply = "error",
-                invalidConfigPath = "error",
-                invalidScreen = "error",
-                invalidTailwindDirective = "error",
-                invalidVariant = "error",
-                recommendedVariantOrder = "warning"
-              },
-              validate = true
-            }
-          },
-          cmd = { "tailwindcss-language-server", "--stdio" },
-          filetypes = { "svelte", "css", "scss", "html", "postcss", "javascript", "typescript" },
-          root_dir = util.root_pattern('tailwind.config.js', 'tailwind.config.cjs', 'tailwind.config.mjs', 'tailwind.config.ts', 'postcss.config.js', 'postcss.config.cjs', 'postcss.config.mjs', 'postcss.config.ts', 'package.json')
-        }
       }
     }
   }
