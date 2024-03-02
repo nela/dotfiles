90d06ce82206abfacd1c29e6037bc21b806d906b
Nemanja Lakicevic <n@lakicevic.no>
2024-03-02 11:59:17 +0100

    feat: add misc plugins

---
+++ nvim/.config/lazy/lua/plugins/colorizer.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'norcalli/nvim-colorizer.lua',
+    cmd = { 'ColorizerAttachToBuffer', 'ColorizerToggle' },
+    opts = {
+      'css';
+      'css_fn';
+      'hsl_fn';
+      'rgb_fn';
+    },
+    main = 'colorizer'
+  }
+}
