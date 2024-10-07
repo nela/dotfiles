ad720bbc3276ef4116c5f52bcb0c6f5e8bbc97b3
Nemanja Lakicevic <nocontact>
2024-10-07 13:10:53 +0200

    feat: clang conf

--- nvim/.config/lazy/lua/ft/clang.lua
+++ nvim/.config/lazy/lua/ft/clang.lua
@@ -3,10 +3,18 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        c = { "clang-format" }
+        c = { "clang-format" },
         cpp = { "clang-format" }
       }
     },
+  },
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    config = function() end,
+    opts = {
+      inlay_hints = { inline = false }
+    }
   },
   {
     "neovim/nvim-lspconfig",
@@ -47,11 +55,50 @@ return {
             clangdFileStatus = true,
           },
         },
+      },
+      setup = {
+        clangd = function(_, opts)
+          local clangd_ext_ops = require("util").opts("clangd_extensions.nvim")
+          require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_ops or {}, { server = opts }))
+          return false -- why false?
+        end
       }
     }
   },
   {
     "mfussenegger/nvim-dap",
-    opts = functinn
+    opts = function()
+      local dap = require("dap")
+      require("dap").adapters["codelldb"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = os.getenv("XDG_DATA_HOME") .. 'codelldb/adapter/bin/codelldb',
+          args = { "--port", "${port}" }
+        }
+      }
+
+      for _, lang in ipairs({ "c", "cpp"--[[ , "rust"  ]]}) do
+        dap.configurations[lang] = {
+          {
+            type = "codelldb",
+            request = "launch",
+            name = "Launch file",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}"
+          },
+          {
+            type = "codelldb",
+            request = "attach",
+            name = "Attach to process",
+            pid = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}"
+          }
+        }
+      end
+    end
   }
 }
