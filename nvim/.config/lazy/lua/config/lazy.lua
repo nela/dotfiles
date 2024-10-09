769ea67c846f34797d458aea1a20dcaec99f62c1
Nemanja Lakicevic <nocontact>
2024-10-09 09:36:14 +0200

    fix: rm depth from lazy clone

--- nvim/.config/lazy/lua/config/lazy.lua
+++ nvim/.config/lazy/lua/config/lazy.lua
@@ -1,13 +1,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
     		"clone",
     		"--filter=blob:none",
-    		-- "git@github.com:folke/lazy.nvim.git",
-        "https://github.com/folke/lazy.nvim.git",
-    		"--depth=1",
+    		"git@github.com:folke/lazy.nvim.git",
+        -- "https://github.com/folke/lazy.nvim.git",
     		"--branch=stable", -- latest stable release
     		lazypath,
 	})
